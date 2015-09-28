7e368239247ee573264f26475f241131c8b6aafd
JD Wong <jdmswong@gmail.com>
2015-09-28 10:22:54 -0700

    removed comment

--- client/sideMenu/sideMenu_controllers.js
+++ client/sideMenu/sideMenu_controllers.js
@@ -3,11 +3,6 @@ angular.module('convosLive')
 .controller("MenuCtrl", function($scope, $state, $rootScope, $meteor){
 
     $scope.$on('$ionicView.enter', function(e) {
-      // I need a way to reactively track the current user.  The
-      // resolved currentUser doesn't change on logging out and back
-      // in as someone else.  Current solution degrades performance
-      // TODO: find a proper solution
-      // Tracker.autorun() with Meteor.user() perhaps
       if($rootScope.currentUser)
         $scope.me = $rootScope.currentUser;
       else
